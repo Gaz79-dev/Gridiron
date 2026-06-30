49e4e223f5bfee94ee2767eff936bd5393b4958d
Gaz <gary.aitchison@outlook.com>
2026-06-30 17:32:26 +0100

    Update __init__.py

--- bot/game_systems/__init__.py
+++ bot/game_systems/__init__.py
@@ -1,5 +1,5 @@
 """Game system definitions used by signup, templates, and squad building."""
 
-from .registry import DEFAULT_GAME_ID, get_game_system, list_game_systems
+from .registry import DEFAULT_GAME_ID, get_all_game_systems, get_game_system, list_game_systems
 
-__all__ = ["DEFAULT_GAME_ID", "get_game_system", "list_game_systems"]
+__all__ = ["DEFAULT_GAME_ID", "get_all_game_systems", "get_game_system", "list_game_systems"]
