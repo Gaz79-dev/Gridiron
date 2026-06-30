bd8505dffb5489d106204ad7cebcd92266dd823e
Gaz <gary.aitchison@outlook.com>
2026-06-30 17:08:25 +0100

    Create __init__.py

---
+++ bot/game_systems/__init__.py
@@ -0,0 +1,5 @@
+"""Game system definitions used by signup, templates, and squad building."""
+
+from .registry import DEFAULT_GAME_ID, get_game_system, list_game_systems
+
+__all__ = ["DEFAULT_GAME_ID", "get_game_system", "list_game_systems"]
